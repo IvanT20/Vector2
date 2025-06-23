45c95314cd27c857606030c4450f7505afb1164b
Ivan Tat <ivtat3048@gmail.com>
2025-06-23 01:42:43 -0400

    Added attributes and fixed logic error with == operator

--- vector2.hpp
+++ vector2.hpp
@@ -14,61 +14,61 @@ public:
     double y = 0;
 
     Vec2() = default;
-    Vec2(double x, double y):x(x), y(y){};
-    
+    Vec2(double x, double y) :x(x), y(y){};
+
     /**
      * @brief Compares this Vec2 with another Vec2 for approximate equality.
-     * 
-     * @param vecTwo The Vec2 to be compared against. 
-     * @return True if the vectors are equal within an epsilon threshold; false otherwise. 
+     *
+     * @param vecTwo The Vec2 to be compared against.
+     * @return True if the vectors are equal within an epsilon threshold; false otherwise.
      */
     inline bool operator==(const Vec2& vecTwo) const
     {
-        constexpr double relEpsilon = 1e-8; 
+        constexpr double relEpsilon = 1e-8;
         constexpr double absEpsilon = 1e-12; // Maximum tolerance allowed to compare floats
 
-        if (std::abs(this->x - vecTwo.x) <= absEpsilon && std::abs(this->y - vecTwo.y <= absEpsilon))
+        if (std::abs(this->x - vecTwo.x) <= absEpsilon && std::abs(this->y - vecTwo.y) <= absEpsilon)
         {
             return true;
         }
 
-        return std::abs(this->x - vecTwo.x) <= std::max(std::abs(this->x), std::abs(vecTwo.x)) * relEpsilon && 
-               std::abs(this->y - vecTwo.y) <= std::max(std::abs(this->y), std::abs(vecTwo.y)) * relEpsilon;
+        return std::abs(this->x - vecTwo.x) <= std::max(std::abs(this->x), std::abs(vecTwo.x)) * relEpsilon &&
+            std::abs(this->y - vecTwo.y) <= std::max(std::abs(this->y), std::abs(vecTwo.y)) * relEpsilon;
     }
 
     /**
      * @brief Negation of the comparison check.
-     * 
+     *
      * @param vecTwo The Vec2 to be compared against.
      * @return True if the two vectors are not equal; false otherwise.
      */
-    inline bool operator!=(const Vec2& vecTwo) const 
+    inline bool operator!=(const Vec2& vecTwo) const
     {
         return !(*this == vecTwo);
     }
-    
+
     /**
-     * @brief Adds this Vec2 with another Vec2 and stores it into the first Vec2. 
-     * 
-     * @param vecTwo The Vec2 that will be used to add to the first Vec2. 
-     * @return The original Vec2 with the added value. 
+     * @brief Adds this Vec2 with another Vec2 and stores it into the first Vec2.
+     *
+     * @param vecTwo The Vec2 that will be used to add to the first Vec2.
+     * @return The original Vec2 with the added value.
      */
-    inline Vec2& operator+=(const Vec2& vecTwo) 
+    inline Vec2& operator+=(const Vec2& vecTwo)
     {
-        this->x += vecTwo.x; 
+        this->x += vecTwo.x;
         this->y += vecTwo.y;
-        
+
         return *this;
     }
 
     /**
      * @brief Add this Vec2 with another Vec2.
-     * 
+     *
      * @param vecOne The Vec2 that will be added to.
      * @param vecTwo The Vec2 that will be used to add to the first Vec2.
      * @return A Vec2 with the added value.
      */
-    friend inline Vec2 operator+(Vec2 vecOne, const Vec2& vecTwo)
+    [[nodiscard]] friend inline Vec2 operator+(Vec2 vecOne, const Vec2& vecTwo)
     {
         vecOne += vecTwo;
         return vecOne;
@@ -76,9 +76,9 @@ public:
 
     /**
      * @brief Subtracts this Vec2 with another Vec2 and stores it into the first Vec2.
-     * 
-     * @param vecTwo The Vec2 that will be used to subtract the first Vec2. 
-     * @return The original Vec2 with the subtracted value. 
+     *
+     * @param vecTwo The Vec2 that will be used to subtract the first Vec2.
+     * @return The original Vec2 with the subtracted value.
      */
     inline Vec2& operator-=(const Vec2& vecTwo)
     {
@@ -89,26 +89,26 @@ public:
     }
 
     /**
-     * @brief Subtracts this Vec2 with another Vec2. 
-     * 
-     * @param vecOne The Vec2 that will be subtracted from. 
-     * @param vecTwo The Vec2 that will be use to subtract the first Vec2. 
-     * @return A Vec2 with the subtracted value. 
+     * @brief Subtracts this Vec2 with another Vec2.
+     *
+     * @param vecOne The Vec2 that will be subtracted from.
+     * @param vecTwo The Vec2 that will be use to subtract the first Vec2.
+     * @return A Vec2 with the subtracted value.
      */
-    friend inline Vec2 operator-(Vec2 vecOne, const Vec2 vecTwo)
+    [[nodiscard]] friend inline Vec2 operator-(Vec2 vecOne, const Vec2& vecTwo)
     {
         vecOne -= vecTwo;
         return vecOne;
     }
 
     /**
-     * @brief Normalizes Vec2. 
-     * 
-     * @attention This function does not check if the components of Vec2 are 0 before normalizing. 
+     * @brief Normalizes Vec2.
+     *
+     * @attention This function does not check if the components of Vec2 are 0 before normalizing.
      * @return The original Vec2 normalized.
      */
     inline Vec2& normalize()
-    {   
+    {
         double magnitude = this->length();
         this->x /= magnitude;
         this->y /= magnitude;
@@ -117,10 +117,10 @@ public:
 
     /**
      * @brief Calculates the length/magnitude of this Vec2.
-     * 
+     *
      * @return The length/magnitude of this Vec2.
      */
-    inline double length() const 
+    [[nodiscard]] inline double length() const
     {
         return std::sqrt(this->x * this->x + this->y * this->y);
     }
